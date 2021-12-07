83564e493a9a0573bd08e4716223cb362304408d
kevinlai219 <kevinvanlet@gmail.com>
2021-12-07 02:21:56 -0600

    add redux-saga test functionality

---
+++ sagas.spec.js
@@ -0,0 +1,29 @@
+import test from 'tape'
+
+import { incrementAsync, delay } from './sagas'
+
+test('incrementAsync Saga test', (assert) => {
+    const gen = incrementAsync()
+
+    assert.deepEqual(
+        gen.next().value,
+        call(delay, 1000),
+        'incrementAsync Sagga must call delay(1000)'
+    )
+
+    assert.deepEqual(
+        gen.next().value,
+        put({ type: 'INCREMENT' }),
+        'incrementAsync Saga must dispatch an INCREMENT action'
+    )
+
+    assert.deepEqual(
+        gen.next(),
+        {
+            done: true, value: undefined
+        },
+        'incrementAsync Saga must be done'
+    )
+
+    assert.end()
+})
